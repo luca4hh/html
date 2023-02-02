a2b49df3ab5fe432255b7f3f813fe3ebbe299d32
lucarichardsen <luca.work@web.de>
2023-02-02 20:27:52 +0100

    All works

--- script.js
+++ script.js
@@ -1,85 +1,123 @@
-/* TITLE TEXT ANIMATION */
+/* TITLE TEXT ANIMATION muss in eine schleife da er sonst komplett abbricht*/
+/* DARKMODE GEHT IMMER NOCH NICHT */
+
+function HackedText() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let interval = null;
+  document.getElementById("hacked-effect").onmouseover = (event) => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+  };
+}
+
+/* OLD
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 let interval = null;
-
-document.querySelector("h1").onmouseover = event => {  
+document.querySelector("#hacked-effect").onmouseover = (event) => {
   let iteration = 0;
-  
+
   clearInterval(interval);
-  
+
   interval = setInterval(() => {
     event.target.innerText = event.target.innerText
       .split("")
       .map((letter, index) => {
-        if(index < iteration) {
+        if (index < iteration) {
           return event.target.dataset.value[index];
         }
-      
-        return letters[Math.floor(Math.random() * 26)]
+
+        return letters[Math.floor(Math.random() * 26)];
       })
       .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
+
+    if (iteration >= event.target.dataset.value.length) {
       clearInterval(interval);
     }
-    
+
     iteration += 1 / 3;
   }, 30);
-}
+};
+*/
 
 /* TITLE TEXT ANIMATION END */
 
 /* DARKMODE */
+const darkModeBody = document.querySelector("body");
+var themeMode = localStorage.getItem("theme");
 
-const darkModeBtn = document.querySelector('.theme-switch');
-const darkModeBody = document.querySelector('body');
-
-const darkMode = () => {
-    darkModeBody.classList.toggle('dark')
+// Listening on click event
+function initPage() {
+  const darkModeBtn = document.querySelector(".theme-switch");
+  darkModeBtn.addEventListener("click", toggelTheme);
+  checkTheme();
 }
 
-darkModeBtn.addEventListener('click', () => {
-    setDarkMode = localStorage.getItem('dark');
+// Toggel from light to dark or in the other way + add the data to localstorage
+function toggelTheme() {
+  themeMode = localStorage.getItem("theme");
+  // Wenn themeMode off ist bodyclass remove "dark"
+  if (themeMode === "light") {
+    darkModeBody.classList.add("dark");
+    themeMode = localStorage.setItem("theme", "dark");
+  } else {
+    darkModeBody.classList.remove("dark");
+    themeMode = localStorage.setItem("theme", "light");
+  }
+}
 
-    if(setDarkMode !== "on") {
-        darkMode();
-        setDarkMode = localStorage.setItem('dark', 'on');
-    } else {
-        darkMode();
-        setDarkMode = localStorage.setItem('dark', null);
-    }
-});
-
-let setDarkMode = localStorage.getItem('dark');
-
-if(setDarkMode === 'on') {
-    darkMode();
+function checkTheme() {
+  themeMode = localStorage.getItem("theme");
+  if (themeMode === "light") {
+    darkModeBody.classList.remove("dark");
+  } else {
+    darkModeBody.classList.add("dark");
+  }
 }
 
 /* DARKMODE END */
 
 /* GALLERY */
 
-const images = document.querySelectorAll(".galerie img");
+const images = document.querySelectorAll(".galerie .img-gallery");
 
 document.addEventListener("mousemove", ($event) => {
-	const { clientX } = $event;
-	const percent = calcPercent(clientX);
-	images.forEach((image) => moveImageBackground(image, percent));
+  const { clientX } = $event;
+  const percent = calcPercent(clientX);
+  images.forEach((image) => moveImageBackground(image, percent));
 });
 
 function calcPercent(clientX) {
-	return (clientX / window.innerWidth) * 100;
+  return (clientX / window.innerWidth) * 100;
 }
 
 function moveImageBackground(image, percent) {
-	image.animate(
-		{
-			objectPosition: `${percent}% 0%`
-		},
-		{ fill: "forwards", duration: 250, easing: "ease-in" }
-	);
+  image.animate(
+    {
+      objectPosition: `${percent}% 0%`,
+    },
+    { fill: "forwards", duration: 250, easing: "ease-in" }
+  );
 }
 
 /* GALLERY END */
